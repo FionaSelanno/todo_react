3aff1795a6dea0d30ea67b73c5e1123f7a27c217
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-08 07:41:23 +0200

    imported jquery and created ToDoFeed component

--- src/ToDoFeed.js
+++ src/ToDoFeed.js
@@ -1,2 +1,38 @@
 // every components imported in this file
 import React from 'react';
+import $ from 'jquery';
+
+
+class ToDoFeed extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: "Loading...",
+      todoItem: []
+    };
+}
+  renderToDoItem(item, index) {
+   console.log(item);
+   return (
+     <h2></h2>
+   );
+ }
+
+  render() {
+    let title = this.state.title;
+    let todoItem = this.state.todoItems;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+
+        <div>
+          {todoItems.map(this.renderToDoItem.bind(this))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ToDoFeed;
